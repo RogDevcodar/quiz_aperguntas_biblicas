32dceac7d9084750608129011c64ca5386ffdca4
Roger-Melo <rogerw592@gmail.com>
2023-08-05 13:33:51 -0300

    Sugestão de refactoring

--- app.js
+++ app.js
@@ -1,90 +1,71 @@
-//Referenciamento do formulário
 const form = document.querySelector('.form-container');
-
-//Referenciamento da div da score
 const divScore = document.querySelector('.divScore');
-
-//Referenciamento do segundo parágrafo da popup
 const messagePoints = document.querySelector('.message-points');
-
-//Referenciamento da tela de fundo acizentada da popup
 const section = document.querySelector('.background-popup');
-
-//Referenciamento do botão de popup
 const buttonPopup = document.querySelector('.button-popup');
 
-//Array com as respostas corretas
-const arrayAnswersCorrect = ['B', 'A', 'D', 'B', 'B', 'B', 'A', 'A', 'D', 'A'];
+const correctAnswers = ['B', 'A', 'D', 'B', 'B', 'B', 'A', 'A', 'D', 'A'];
 
-//Evento de submit do botão
-form.addEventListener('submit', event => {
-    event.preventDefault(); //Evita que a página seja carregada ao clicar no botão.
-    scrollTo(0, 0);        //Ao clicar no botão a página retorna ao eixo x(0) e y(0).
-    
-
-//Consts contendo as mensagens que avalia as respostas do usuário.
-
-
-    //Array com os checkeds dos inputs clicados pelo usuário.
-    const arrayCheckedUsers = [
-        form.flexRadioDefault1.value,
-        form.flexRadioDefault2.value,
-        form.flexRadioDefault3.value,
-        form.flexRadioDefault4.value,
-        form.flexRadioDefault5.value, 
-        form.flexRadioDefault6.value, 
-        form.flexRadioDefault7.value, 
-        form.flexRadioDefault8.value, 
-        form.flexRadioDefault9.value, 
-        form.flexRadioDefault10.value];
-
-//lets do contador da pontuação.
-    let score = 0;
-//lets do contador da animação da pontuação.
-    let countPoints = 0;    
-
-//Const referente ao forEach que itera pelo array 'arrayCheckedUsers'.  
-const sumScore = (arrayCheckedUser, index) => {
-    if (arrayCheckedUser === arrayAnswersCorrect[index]) {
-        score += 10;
-    }
+const getUserAnswers = () => {
+  const userAnswers = []
+  
+  correctAnswers.forEach((_, i) => 
+    userAnswers.push(form[`flexRadioDefault${i + 1}`].value))
+  
+  return userAnswers
 }
-//forEach da const sumScore
-arrayCheckedUsers.forEach(sumScore)
 
-//Ao clicar no submit a classe d-none é removida para o score ser exibido na popup.
-section.classList.remove('d-none');
-
-// Animação da pontuação do quiz
-    const timerCount = setInterval(() => {
-                   
-        if (countPoints === score) {
-        clearInterval(timerCount); // Para a animação ao atingir a pontuação do usuário.
+const getUserScore = userAnswers => {
+  let score = 0;
+  
+  userAnswers.forEach((userAnswer, index) => {
+    if (userAnswer === correctAnswers[index]) {
+      score += 10;
     }
-        divScore.querySelector('span').textContent = `${countPoints}%`;
-        countPoints++;
-    },10)  
+  })
 
-//Consts da condicional de avaliação das respostas
-const smallerOrEqualThirty = score <= 30;
-const bigThirtySmallerSixty = score > 30 && score <= 60;
-const bigSixtySmallerNinety = score > 60 && score <= 90;
-const bigNinetySmallerHundred = score > 90 && score <= 100;
+  return score
+}
 
-//Condicional de avaliação das respostas
-    if (smallerOrEqualThirty) {
-       messagePoints.textContent = 'Precisa estudar mais! Mas com esforço você consegue.'
-    }   else if (bigThirtySmallerSixty) {
-        messagePoints.textContent = 'Não está mal, mas ainda há espaço para melhorar.'
-    }   else if (bigSixtySmallerNinety) {
-        messagePoints.textContent = 'Muito bem! Você tem um bom conhecimento geral.'
-    }   else if (bigNinetySmallerHundred){
-        messagePoints.textContent = 'Parabéns! Você é um especialista em assuntos gerais!'
+const getScoreMessage = score => ({
+  10: 'Precisa estudar mais! Mas com esforço você consegue.',
+  20: 'Precisa estudar mais! Mas com esforço você consegue.',
+  30: 'Precisa estudar mais! Mas com esforço você consegue.',
+  40: 'Não está mal, mas ainda há espaço para melhorar.',
+  50: 'Não está mal, mas ainda há espaço para melhorar.',
+  60: 'Não está mal, mas ainda há espaço para melhorar.',
+  70: 'Muito bem! Você tem um bom conhecimento geral.',
+  80: 'Muito bem! Você tem um bom conhecimento geral.',
+  90: 'Muito bem! Você tem um bom conhecimento geral.'
+})[score] || 'Parabéns! Você é um especialista em assuntos gerais!'
+
+const showScore = score => {
+  let countPoints = 0;
+  
+  section.classList.remove('d-none');
+
+  const timerCount = setInterval(() => {
+    if (countPoints === score) {
+      clearInterval(timerCount);
     }
-});
 
-// Evento de  click que fecha a popup.
-buttonPopup.addEventListener('click', () => {
+    divScore.querySelector('span').textContent = `${countPoints}%`;
+    countPoints++;
+  }, 10)
 
-    section.classList.add('d-none');
-})
+  const scoreMessage = getScoreMessage(score)
+  messagePoints.textContent = scoreMessage
+}
+
+const handleFormSubmit = event => {
+  event.preventDefault();
+  scrollTo(0, 0);
+
+  const userAnswers = getUserAnswers()
+  const score = getUserScore(userAnswers)
+
+  showScore(score)
+}
+
+form.addEventListener('submit', handleFormSubmit);
+buttonPopup.addEventListener('click', () => section.classList.add('d-none'))
